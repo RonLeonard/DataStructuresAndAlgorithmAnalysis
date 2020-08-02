8e355fcb196c47588fe82435de2a890d609c09e3
Miao.Ren <renmiaorm@163.com>
2020-08-02 21:52:56 +0800

    add the linked list code that has guard node

---
+++ list/linkedlist/guardlinkedlist.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include "guardlinkedlist.h"
+CGuardLinkedList::CGuardLinkedList()
+	: mSize(1)
+{
+	head = new stListNode();
+	head->data = 0;
+	head->pNext = nullptr;
+}
+
+
+CGuardLinkedList::~CGuardLinkedList()
+{
+	while (head != nullptr)
+	{
+		stListNode* tmpNode = head;
+		head = head->pNext;
+		delete tmpNode;
+		tmpNode = nullptr;
+	}
+	mSize = 0;
+}
+
+
+stListNode* CGuardLinkedList::findFirstPos(int val)
+{
+	stListNode* scout_solider = head->pNext;
+	while (scout_solider != nullptr)
+	{
+		if (scout_solider->data == val)
+			return scout_solider;
+		scout_solider = scout_solider->pNext;
+	}
+	std::cout << "there is no the specified value in the linked list." << std::endl;
+	return nullptr;
+}
+
+
+stListNode* CGuardLinkedList::findVal(unsigned int idx)
+{
+
+
+}
+
+
+bool CGuardLinkedList::delNode(int val)
+{
+	bool ret = false;
+	if (head->pNext == nullptr)
+	{
+		std::cout << "the linked list is null." << std::endl;
+		return false;
+	}
+
+	stListNode* scout_solider = head;
+	while (scout_solider->pNext != nullptr)
+	{
+		if (scout_solider->pNext->data == val)
+		{
+			stListNode* tmpNode = scout_solider->pNext;
+			scout_solider->pNext = scout_solider->pNext->pNext;
+			delete tmpNode;
+			tmpNode = nullptr;
+			mSize -= 1;
+			ret = true;
+		}
+		else
+			scout_solider = scout_solider->pNext;
+	}
+	return ret;
+}
+
+
+bool CGuardLinkedList::delNode(unsigned int idx)
+{
+	if (mSize <= 1)
+	{
+		std::cout << "the linked list is null." << std::endl;
+		return false;
+	}
+
+	if (idx >= mSize || idx == 0)
+	{
+		std::cout << "the index beyond of the size of the linked list." << std::endl;
+		return false;
+	}
+
+	stListNode* scout_solider = head;
+	unsigned int tmpIdx = 1;
+	while (scout_solider->pNext != nullptr)
+	{
+		if (tmpIdx == idx)
+		{
+			stListNode* tmpNode = scout_solider->pNext;
+			scout_solider->pNext = scout_solider->pNext->pNext;
+			delete tmpNode;
+			tmpNode = nullptr;
+			return true;
+		}
+		tmpIdx++;
+		scout_solider = scout_solider->pNext;
+	}
+}
+
+
+bool CGuardLinkedList::insertNode(stListNode* pStListNode, unsigned int idx)
+{
+	if (idx > mSize)
+	{
+		std::cout << "the index beyond of the size of the linked list." << std::endl;
+		return false;
+	}
+	if (idx == 0)
+	{
+		std::cout << "the index must bigger than zero." << std::endl;
+		return false;
+	}
+	stListNode* scout_solider = head;
+	while (idx--)
+	{
+		scout_solider = scout_solider->pNext;
+	}
+
+	pStListNode->pNext = scout_solider->pNext;
+	scout_solider->pNext = pStListNode;
+	return true;
+}
+
+
+void CGuardLinkedList::printLinkedList()
+{
+	if (head == nullptr)
+	{
+		std::cout << "the linked list is null." << std::endl;
+	}
+	stListNode* scout_solider = head->pNext;
+	int index = 1;
+	while (scout_solider != nullptr)
+	{
+		std::cout << "the no. " << index << " of the linked list's value is " << scout_solider->data << std::endl;
+		scout_solider = scout_solider->pNext;
+		index++;
+	}
+}
---
+++ list/linkedlist/guardlinkedlist.h
@@ -0,0 +1,33 @@
+#ifndef GUARD_LINKED_LIST_H_
+#define GUARD_LINKED_LIST_H_
+
+
+typedef struct {
+	int data;
+	stListNode* pNext;
+} stListNode;
+
+
+
+class CGuardLinkedList
+{
+
+public:
+	CGuardLinkedList();
+	~CGuardLinkedList();
+	stListNode* findFirstPos(int val);
+	stListNode* findVal(unsigned int idx);
+
+	bool delNode(int val);
+	bool delNode(unsigned int idx);
+
+	bool insertNode(stListNode* pStListNode, unsigned int idx);
+	void printLinkedList();
+private:
+	unsigned int mSize;
+	stListNode* head;
+};
+
+
+
+#endif
