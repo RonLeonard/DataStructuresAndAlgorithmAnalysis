65e806231aca0b7311f61eea5636494cc810f2db
Miao.Ren <Miao.Ren@desay-svautomotive.com>
2021-05-08 10:19:21 +0800

    add the singlelinkedlist of third excercise
    Signed-off-by: Miao.Ren <Miao.Ren@desay-svautomotive.com>

--- ThirdExcercise/linked_list_test/linked_list_test/single_linkedlist.cpp
+++ ThirdExcercise/linked_list_test/linked_list_test/single_linkedlist.cpp
@@ -52,8 +52,97 @@ bool CSingleLinkedList::delVal(const int& val)
 	}
 	return false;
 }
-bool CSingleLinkedList::delNode(const StNode* pNode);
-bool CSingleLinkedList::insertBefore(const StNode* pNode, const int& val);
-bool CSingleLinkedList::insertAfter(const StNode* pNode, const int& val);
-bool CSingleLinkedList::insertHead(const int& val);
-bool CSingleLinkedList::insertTail(const int& val);
+
+
+bool CSingleLinkedList::delNode(const StNode* pNode)
+{
+	if (pNode == nullptr || head == nullptr)
+		return false;
+	StNode* prev = head;
+	StNode* tmp = head->next;
+	while (tmp != nullptr)
+	{
+		if (tmp == pNode)
+		{
+			prev->next = tmp->next;
+			delete tmp;
+			return true;
+		}
+		prev = tmp;
+		tmp = tmp->next;
+	}
+	return false;
+}
+
+
+bool CSingleLinkedList::insertBefore(const StNode* pNode, const int& val)
+{
+	if (pNode == nullptr || head == nullptr)
+		return false;
+	StNode* prev = head;
+	StNode* tmp = head->next;
+	while (tmp != nullptr)
+	{
+		if (tmp == pNode)
+		{
+			StNode* insNode = new StNode();
+			insNode->data = val;
+			insNode->next = prev->next;
+			prev->next = insNode;
+			return true;
+		}
+		prev = tmp;
+		tmp = tmp->next;
+	}
+	return false;
+}
+
+
+bool CSingleLinkedList::insertAfter(const StNode* pNode, const int& val)
+{
+	if (pNode == nullptr || head == nullptr)
+		return false;
+	StNode* tmp = head->next;
+	while (tmp != nullptr)
+	{
+		if (tmp == pNode)
+		{
+			StNode* insNode = new StNode();
+			insNode->data = val;
+			insNode->next = tmp->next;
+			tmp->next = tmp;
+			return true;
+		}
+		tmp = tmp->next;
+	}
+	return false;
+}
+
+
+bool CSingleLinkedList::insertHead(const int& val)
+{
+	if (head == nullptr)
+		return false;
+	StNode* insNode = new StNode();
+	insNode->data = val;
+	insNode->next = head->next;
+	head->next = insNode;
+	return true;
+}
+
+
+bool CSingleLinkedList::insertTail(const int& val)
+{
+	if (head == nullptr)
+		return false;
+	StNode* insNode = new StNode();
+	insNode->data = val;
+	insNode->next = nullptr;
+	StNode* tmp = head;
+	while (tmp->next != nullptr)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = insNode;
+	return true;
+}
